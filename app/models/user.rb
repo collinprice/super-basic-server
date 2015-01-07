e478926012e769d675f974fc3675304ef7440415
Collin Price <collin@collinprice.com>
2015-01-06 20:49:41 -0500

    init

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+
+  devise :database_authenticatable, :recoverable,
+         :trackable, :validatable, :registerable,
+         :omniauthable
+         
+  include DeviseTokenAuth::Concerns::User
+
+  before_save :skip_confirmation!
+end
